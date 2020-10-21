75a600c2ec7178c664a0d35de60cbe36d01e02e5
Matheus Amaral <matheusamaral-99@hotmail.com>
2020-10-21 10:34:41 -0300

    Aula 21/10 configurando conexao BD

--- testes/testeDb.js
+++ testes/testeDb.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
-const dbConfig = require("../config/database");
-const dbConn = new Sequelize(dbConfig);
+const dbConfig = require("../config/config.json");
+const dbConn = new Sequelize(dbConfig.development);
 
 dbConn.query("select * from usuarios", Sequelize.QueryTypes.SELECT)
 .then(
